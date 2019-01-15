4789108cade7566f19695d23f395254159ed8d7b
csPython <cspython@AD25TF04NGF1J-40.local>
2019-01-15 11:22:38 -0500

    2nd question added, score adjusted

---
+++ quizRN.py
@@ -0,0 +1,51 @@
+#varibles
+q1C = False
+q2C = False
+grade = 0
+
+#intro
+print("you will answer this multiple chocie question")
+print("only chose numbers 1=4")
+
+#question 1
+print("""what is 2+2?
+1)4
+2)5
+3)7
+4)2""")
+
+#code
+while q1C == False:
+    try:
+        q1 = int(input())
+        if 0 < q1 < 5:
+            q1C = True
+            if q1 == 1:
+                grade+=1
+        else:
+            print("your amswer was not a number 1-4")
+    except ValueError:
+        print("your answer was a letter not a number 1-4")
+
+#question 2
+print("""what is 10+20-10?
+1)10
+2)30
+3)20
+4)40""")
+
+#code
+while q2C == False:
+    try:
+        q2 = int(input())
+        if 0 < q2 < 5:
+            q2C = True
+            if q2 == 3:
+                grade+=1
+        else:
+            print("your amswer was not a number 1-4")
+    except ValueError:
+        print("your answer was a letter not a number 1-4")
+
+    print("your score is" , grade *50 , "% out of 100%")
+        
