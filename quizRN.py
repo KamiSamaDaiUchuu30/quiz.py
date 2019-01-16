0859fe1692c2d30b6e3ee06a872d2881e98472b1
csPython <cspython@AD25TF04NGF1J-40.local>
2019-01-16 12:18:31 -0500

    custom function defined, score commented out

--- quizRN.py
+++ quizRN.py
@@ -1,29 +1,33 @@
-#varibles
+
+#custom function
+def run_quest(quest, check, ansU, ansR):
+    print(quest)
+    while check == False:
+        try:
+            ansU = int(input())
+            if ansU == ansR:
+                #grade += 1
+                check = True
+            elif 0 < ansU < 5:
+                check = True
+            else:
+                print("your amswer was not a number 1-4")
+        except ValueError:
+           print("your answer was a letter not a number 1-4")
+
+#q1 data
 q1C = False
-grade = 0
-
-#intro
-print("you will answer this multiple chocie question")
-print("only chose numbers 1=4")
-
-#question 1
-print("""what is 2+2?
+q1T = str("""what is 2+2?
 1)4
 2)5
 3)7
 4)2""")
+q1R = 1
 
-#code
-while q1C == False:
-    try:
-        q1 = int(input())
-        if 0 < q1 < 5:
-            q1C = True
-            if q1 == 1:
-                grade+=100
-        else:
-            print("your amswer was not a number 1-4")
-    except ValueError:
-        print("your answer was a letter not a number 1-4")
+#runs the code
+run_quest(q1T,q1C,0,q1R)
 
-    print("your score is" , grade , "% out of 100%")
+#gives user there score
+#print("your score is" , grade * 50, "% out of 100%")
+
+
