74a66ae2ee926353cd49c3485e8ca46f5b559c83
csPython <cspython@AD25TF04NGF1J-40.local>
2019-01-16 12:23:32 -0500

    2 questions, global variable, working score

--- quizRN.py
+++ quizRN.py
@@ -1,3 +1,5 @@
+#global varible that works for the whole program 
+grade = 0
 
 #custom function
 def run_quest(quest, check, ansU, ansR):
@@ -6,7 +8,8 @@ def run_quest(quest, check, ansU, ansR):
         try:
             ansU = int(input())
             if ansU == ansR:
-                #grade += 1
+                global grade
+                grade += 1
                 check = True
             elif 0 < ansU < 5:
                 check = True
@@ -24,10 +27,20 @@ q1T = str("""what is 2+2?
 4)2""")
 q1R = 1
 
+#q2 data
+q2C = False
+q2T = str("""what is 10+20-10?
+1)30
+2)40
+3)20
+4)10""")
+q2R = 3
+
 #runs the code
 run_quest(q1T,q1C,0,q1R)
+run_quest(q2T,q2C,0,q2R)
 
 #gives user there score
-#print("your score is" , grade * 50, "% out of 100%")
+print("your score is" , grade * 50, "% out of 100%")
 
 
