29e25d7af9956a060b6885371e43c5898be19a47
csPython <cspython@AD25TF04NGF1J-40.local>
2019-01-16 12:13:33 -0500

    question.py code saved as quiz.py

--- quizRN.py
+++ quizRN.py
@@ -1,44 +1,29 @@
-def run_quest(quest, check, ansU, ansR):
-    print(quest)
-    while check == False:
-        try:
-            ansU = int(input())
-            if ansU == ansR:
-                #grade+=100
-                check = True
-            elif 0 < ansU < 5:
-                check = True
-            else:
-                print("your amswer was not a number 1-4")
-        except ValueError:
-            print("your answer was a letter not a number 1-4")
-
-    #print("your score is" , grade , "% out of 100%")
-
-#q1 data
+#varibles
 q1C = False
-#grade = 0
-q1T = str("""what is 2+2?
+grade = 0
+
+#intro
+print("you will answer this multiple chocie question")
+print("only chose numbers 1=4")
+
+#question 1
+print("""what is 2+2?
 1)4
 2)5
 3)7
 4)2""")
-q1R = 1
 
-#q12 data
-q2C = False
-#grade = 0
-q2T = str("""what is 10+20-10?
-1)30
-2)40
-3)20
-4)10""")
-q2R = 3
+#code
+while q1C == False:
+    try:
+        q1 = int(input())
+        if 0 < q1 < 5:
+            q1C = True
+            if q1 == 1:
+                grade+=100
+        else:
+            print("your amswer was not a number 1-4")
+    except ValueError:
+        print("your answer was a letter not a number 1-4")
 
-
-#runs the code
-run_quest(q1T,q1C,0,q1R)
-run_quest(q2T,q2C,1,q2R)
-
-
-
+    print("your score is" , grade , "% out of 100%")
